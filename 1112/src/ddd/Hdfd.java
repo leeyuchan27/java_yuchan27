1602a9ea29474a7bc912dfdbb7ea06f6b325db1d
leeyuchan27 <dltmdals574@naver.com>
2024-07-05 09:17:03 +0900

    sdddd

---
+++ 1112/src/ddd/Hdfd.java
@@ -0,0 +1,10 @@
+package ddd;
+
+public class Hdfd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int ddlwd;
+	}
+
+}
---
+++ dlaofk/src/dlaofkg/JDED.java
@@ -0,0 +1,10 @@
+package dlaofkg;
+
+public class JDED {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String 가나다라;
+	}
+
+}
